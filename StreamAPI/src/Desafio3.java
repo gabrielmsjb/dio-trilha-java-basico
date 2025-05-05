1fe2ba0e2e99145eb79b90dc4bffd6509061f4f7
gabrielmsjb <gabrielbuchi97@gmail.com>
2025-05-05 16:52:46 -0300

    Desafio3

--- StreamAPI/src/Desafio3.java
+++ StreamAPI/src/Desafio3.java
@@ -1,5 +1,11 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Desafio3 {
 
@@ -7,6 +13,9 @@ public class Desafio3 {
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
+        boolean todosPositivos = numeros.stream().allMatch(n -> n > 0);
+
+        System.out.println(todosPositivos);
     }
 
 }
