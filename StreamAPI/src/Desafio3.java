32ec309e72c50691b5a56f4b3581e833ebcb2759
gabrielmsjb <gabrielbuchi97@gmail.com>
2025-05-05 17:32:32 -0300

    Desafio3Correcoes+Desafio4

--- StreamAPI/src/Desafio3.java
+++ StreamAPI/src/Desafio3.java
@@ -1,11 +1,5 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Desafio3 {
 
